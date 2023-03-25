5a31d9f09f0e56a5420545664442518786cafe65
Thire399 <Thire399@live.dk>
2023-03-26 00:48:26 +0100

    Made PRAUC (accuracy cal is bugged)

--- Loop.py
+++ Loop.py
@@ -10,7 +10,7 @@ import Models as M
 from torchvision import models
 from carbontracker.tracker import CarbonTracker
 from sklearn.model_selection import GridSearchCV
-
+from sklearn.metrics import precision_recall_curve
 os.chdir('/home/thire399/Documents/School/DC-MasterThesis-2023')
 
 ####### PARAMETERS #######
@@ -29,8 +29,9 @@ model.classifier[6] = nn.Linear(in_features=4096, out_features = 2, bias=True)
 #Data parameters
 dataSet      = 'chest_xray'
 datatype     = ''
+os.makedirs('Data/Loss_chest_xray/test', exist_ok = True)
 costumLabel  = '64x64Full'
-
+dev = True
 #model parameters
 patience     = 10 #
 delta        = 1e-7
@@ -40,9 +41,45 @@ learningRate = 1e-5 #add weight decay weight_decay=1e-5
 optimizer    = optim.SGD(model.parameters(), lr = learningRate, momentum = 0.5)#optim.Adam(model.parameters(), lr = learningRate)
 loss_Fun     = nn.CrossEntropyLoss()
 batch_size   = 64
-saveModel    = False
+saveModel    = True
 figSave      = False
 ####### PARAMETERS #######
+
+# Maybe move to another file?
+def PRAUC(pred, Target, ep):
+    '''
+    Takes in output from model and Target labels,
+    and an epsilong to avoid div by 0
+    Calculates the precision and recall for the given model over multiple 
+    thressholds, by using the sklearn implementation
+    https://medium.com/@douglaspsteen/precision-recall-curves-d32e5b290248
+    '''
+#    pred = pred.contiguous().view(-1)
+#    pred = pred.cpu().detach().numpy()
+#    Target = Target.contiguous().view(-1)
+#    Target = Target.cpu().detach().numpy()
+    
+    #rewrite into a function.
+    P, R, T = precision_recall_curve(y_true = Target, probas_pred = pred)
+    fscore = (2 * P * R) / (P + R + ep)
+
+    # locate the index of the largest f score
+    ix = np.argmax(fscore)
+    bestT = T[ix]
+    print('Best Threshold=%f, F-Score=%.3f' % (T[ix], fscore[ix]))
+
+    no_skill = len(Target[Target == 1]) / len(Target)
+    plt.plot([0,1], [no_skill,no_skill], linestyle='--', label='No Skill')
+    plt.plot(R, P, marker='.', label = 'Logistic')
+    legend = plt.legend(loc = 'upper right', frameon = 1)
+    frame = legend.get_frame()
+    frame.set_facecolor('grey')
+    frame.set_edgecolor('black')
+    plt.xlabel ('Recall')
+    plt.ylabel('Precision')
+    plt.title('Precision Recall curve')
+    plt.show()
+    return P[ix], R[ix], bestT
 
 def TrainLoop(
             train_Loader
@@ -55,6 +92,7 @@ def TrainLoop(
             , loss_Fun  = loss_Fun
             , modelSave = saveModel
             , figSave = figSave
+            , dev = dev
             ):
     #tracker = CarbonTracker(epochs=epochs)
     #### -- Set up -- ####
@@ -69,12 +107,20 @@ def TrainLoop(
     os.makedirs(mkPathLoss + '/Figs' , exist_ok = True)
 
     now = time.strftime("%Y%m%d-%H%M%S") #save file as current time stamp - better format to save file?
+    if dev:
+        early_stopping = M.EarlyStopping( patience = patience,
+                                verbose = True,
+                                delta   = delta,
+                                path    =  mkPathLoss + '/test' + '/model' + costumLabel + model._get_name() + now,
+                                saveModel = modelSave)
+    else:
+        early_stopping = M.EarlyStopping( patience = patience,
+                        verbose = True,
+                        delta   = delta,
+                        path    =  mkPathLoss + '/model' + costumLabel + model._get_name() + now,
+                        saveModel = modelSave)
 
-    early_stopping = M.EarlyStopping( patience = patience,
-                            verbose = True,
-                            delta   = delta,
-                            path    =  mkPathLoss + '/model' + costumLabel + model._get_name() + now,
-                            saveModel = False)
+        
     train_Loss = []
     val_Loss = []
     batchTrain_loss = []
@@ -133,13 +179,15 @@ def TrainLoop(
         #tracker.stop()
     #### -- Save info -- ####
     if modelSave == True:
-
         t_Loss = torch.tensor(train_Loss)
         v_Loss = torch.tensor(val_Loss)
+        if dev:
+            torch.save(t_Loss, f = mkPathLoss + '/test' + '/train_loss' + costumLabel + model._get_name()  + now + '.pt') # add name
+            torch.save(v_Loss, f = mkPathLoss + '/test' + '/val_loss' + costumLabel + model._get_name() + now + '.pt')   # add name
+        else:
+            torch.save(t_Loss, f = mkPathLoss + '/train_loss' + costumLabel + model._get_name()  + now + '.pt') # add name
+            torch.save(v_Loss, f = mkPathLoss + '/val_loss' + costumLabel + model._get_name() + now + '.pt')   # add name
 
-        torch.save(t_Loss, f = mkPathLoss + '/train_loss' + costumLabel + model._get_name()  + now + '.pt') # add name
-        torch.save(v_Loss, f = mkPathLoss + '/val_loss' + costumLabel + model._get_name() + now + '.pt')   # add name
-        #torch.save(model.state_dict(), mkPathLoss + '/model' + costumLabel + model._get_name() + now + '.pt' ) # saves model.
     plt.plot(range(len(train_Loss)), train_Loss, label = 'Train')
     plt.plot(range(len(val_Loss)), val_Loss, label = 'val')
     plt.axvline(val_Loss.index(min(val_Loss)), linestyle='--', color='r',label='Early Stopping Checkpoint')
@@ -152,9 +200,41 @@ def TrainLoop(
     plt.title('Loss per epoch')
     plt.grid()
     if figSave == True:
-        plt.savefig(mkPathLoss + '/Figs/'+ costumLabel + model._get_name() + now + '.png', bbox_inches='tight', dpi = 400)
+        if dev:
+            plt.savefig(mkPathLoss + '/test' +'/Figs/'+ costumLabel + model._get_name() + now + '.png', bbox_inches='tight', dpi = 400)
+        else:
+            plt.savefig(mkPathLoss + '/Figs/'+ costumLabel + model._get_name() + now + '.png', bbox_inches='tight', dpi = 400)
     plt.show()
-    return None
+
+    #new trying something with PRAUC
+    prediction = []
+    targetList = []
+    if dev:
+        model.load_state_dict(torch.load(mkPathLoss + '/test' + '/model' + costumLabel + model._get_name() + now))
+    else:
+        model.load_state_dict(torch.load(mkPathLoss + '/model' + costumLabel + model._get_name() + now))
+        
+    
+    for batch, (data, target) in enumerate(val_Loader, 1):
+        optimizer.zero_grad() # a clean up step for PyTorch
+        out = model(data.to(device))
+        for p in out.detach().cpu().numpy():
+            prediction.append(np.argmax(p))
+        targetList.append(target.detach().cpu().numpy())
+        if batch % 2 == 0: #For printing
+            print(4*' ', '===> F-Score: [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+                batch * len(data), len(val_Loader.dataset),
+                100. * batch / len(val_Loader),
+                np.mean(batchVal_loss)))
+        break
+    #print('Prediction shape: {0}\n{1}'.format(np.asarray(prediction).shape, prediction))
+    #print('Target shape: {0}\n{1}'.format(np.asarray(targetList).shape, targetList))
+    targetList = np.asarray(targetList).flatten()
+    Precision, Recall, Threshold = PRAUC(prediction, targetList, ep = 1e-5)
+    print('Precision: {0}\nRecall: {1}\nThreshold: {2} (not used)'.format(Precision, Recall, Threshold))
+
+    return prediction, targetList
+
 
 ####### Main Calls ########
 
@@ -188,7 +268,7 @@ val_Loader = torch.utils.data.DataLoader(val_Set,
 #
 #print(grid_result)
 
-TrainLoop(train_Loader = train_Loader
+p , t = TrainLoop(train_Loader = train_Loader
         , val_Loader = val_Loader
         , model    = model
         , patience = patience
@@ -198,6 +278,11 @@ TrainLoop(train_Loader = train_Loader
         , loss_Fun = loss_Fun
         , modelSave = saveModel
         , figSave = figSave
+        , dev = dev
         )
+
+print('Accuracy: {0}'.format((sum([p == t]))/yVal.shape[0]))
+
+
 #train = torch.load('Data/Loss_chest_xray/train_lossResNet20230309-211533.pt').tolist()
 #val = torch.load('Data/Loss_chest_xray/val_lossResNet20230309-211533.pt').tolist()

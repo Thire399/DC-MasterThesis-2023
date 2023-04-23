e5cbd3649e3c32e663377e8227f1c20437c57258
Thire399 <Thire399@live.dk>
2023-04-23 17:01:36 +0200

    Now saves Synthetic tensor, updated parameters

--- CondensationModels.py
+++ CondensationModels.py
@@ -7,9 +7,10 @@ import numpy as np
 import Models as M
 import torch.nn as nn
 import torch.optim as optim
+import random
 from sklearn.metrics.pairwise import rbf_kernel
-import Data_processing as DP
-from torchvision import transforms
+from carbontracker.tracker import CarbonTracker
+
 
 warnings.filterwarnings("ignore")
 
@@ -21,7 +22,10 @@ def Gen_Y(size):
     return torch.cat((class_0, class_1))
 
 class GradientMatching():
-    def __init__(self, model, batchSize, syntheticSampleSize, k, t, c, lr_Theta, lr_S, loss_Fun) -> None:
+    def __init__(self, model, batchSize:int, syntheticSampleSize:int,
+                  k:int, t:int, c:int, lr_Theta:float, lr_S:float
+                  , loss_Fun, DataSet:str, customLabel: str) -> None:
+        
         self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
         self.model = model.to(self.device)
         self.batch_size = batchSize
@@ -29,20 +33,27 @@ class GradientMatching():
         self.t   = t
         self.c   = c
         self.lr_Theta = lr_Theta
+        #now = time.strftime("%Y%m%d-%H%M%S")
+        self.savePath = f'Data/Synthetic_{DataSet}'
+        os.makedirs(f'{self.savePath}/CarbonLogs', exist_ok = True)
+        self.customLabel = customLabel
         self.lr_S = lr_S
-        self.S_x = nn.Parameter(torch.rand((syntheticSampleSize, 3, 128, 128)), requires_grad = True)
+        self.noise = torch.randn(syntheticSampleSize, 3, 128, 128)
+        self.S_x = torch.zeros(syntheticSampleSize, 3, 128, 128, dtype = torch.float)
+        #self.S_x = nn.Parameter(torch.rand((syntheticSampleSize, 3, 128, 128)), requires_grad = True) #Totally random data
         self.S_y = Gen_Y(self.S_x.shape[0])
         self.loss_Fun = loss_Fun
-        self.optimizerT = optim.SGD(self.model.parameters(), lr = lr_Theta)
-        self.optimizerS = optim.SGD([self.S_x], lr = lr_S)
+        self.optimizerT = optim.SGD(self.model.parameters(), lr = lr_Theta, momentum = 0.5)
+        self.optimizerS = optim.SGD([self.S_x], lr = lr_S, momentum = 0.5)
+        self.carbonTracker = CarbonTracker(epochs = self.k*self.t, 
+                            log_dir = self.savePath + '/CarbonLogs',
+                            log_file_prefix = costumLabel + model._get_name(),
+                            monitor_epochs = -1,
+                            update_interval = 0.01
+                            )
         print(f'Setup:\n\tUsing Compute: {self.device}\n\tk = {k}\n\tt = {t}\n\tc = {c}\n\tLearning Rate S: = {lr_S}',
                             f'\tLearning Rate Theta = {lr_Theta}')
-#        print("Parameters:")
-#        for name, param in model.named_parameters():
-#            print(f"{name}: {param.shape}")
-#        print("Parameters:")
-#        for name, param in self.S_x.named_parameters():
-#            print(f"{name}: {param.shape}")
+
         
     def Distance(self, A, B):
         _sum_ = 0
@@ -58,8 +69,8 @@ class GradientMatching():
         return torch.stack([data[i] for i in index])
 
     def GetGradient(self, x, y):
-        #self.optimizerS.zero_grad()
-        #self.optimizerT.zero_grad()
+        #self.optimizerS.zero_grad() #adding this breaks things?
+        #self.optimizerT.zero_grad() #adding this breaks things?
         self.model.train()
         out = self.model(x.to(self.device))
         out = out.flatten()
@@ -74,17 +85,36 @@ class GradientMatching():
                     grad_list.append(l.weight.grad.flatten())
         return grad_list, loss
 
+    def save_output(self, x = None, y = None) -> None:
+        print('Saving synthetic dataset...')
+        if x == None:
+            torch.save(self.S_x, f = f'{self.savePath}/{self.customLabel}X.pt')
+            torch.save(self.S_y, f = f'{self.savePath}/{self.customLabel}Y.pt')
+        else:
+            torch.save(x, f = f'{self.savePath}/{self.customLabel}X.pt')
+            torch.save(y, f = f'{self.savePath}/{self.customLabel}Y.pt')
+        print(f'Saved "{self.customLabel}" to "{self.savePath}"')
+        return None
+
     def Generate(self, T_x, T_y):
-        #S_shape = (size_of_dataset, 1, height, width)
         #Note: assumes binary classification.
         Schange_class_index = torch.argmax(self.S_y).item()
         Tchange_class_index = torch.argmax(T_y).item()
+        
+        c0 = random.sample(range(0, 4032), 400)
+        self.S_y = T_y[c0]
+        self.S_x = T_x[c0]
+        self.S_x = self.S_x + self.noise
+        Schange_class_index = torch.argmax(self.S_y).item()
+        torch.save(self.S_x, f = f'Data/Synthetic_Alzheimer_MRI/BeforeX.pt')
+        torch.save(self.S_y, f = f'Data/Synthetic_Alzheimer_MRI/BeforeY.pt')
+        print(self.S_x.shape, self.S_y.shape)
         for k in range(self.k):
             print('init random weights...')
-            self.model._init_weights() #Here?
+            self.model._init_weights()
             for t in range(self.t):
+                self.carbonTracker.epoch_start()
                 #old = self.S_x.clone()
-                #if t % 2 == 0:
                 print(f'K Iteration: {k}\n\tT Iteration: {t}')
                 for c in range(self.c):
                     print('\t\tGenerating Batches...')
@@ -98,8 +128,7 @@ class GradientMatching():
                         T_DataY = torch.tensor(T_y[Tchange_class_index:])
                         S_DataX = torch.tensor(self.S_x[Schange_class_index:])
                         S_DataY = torch.tensor(self.S_y[Schange_class_index:])
-                        print(T_DataX)
-                    print('\t\tSampling...')
+                    print(f'\t\tSampling for class {c}... ')
                     T_BatchX = self.sampleRandom(T_DataX, batch_size = self.batch_size)
                     T_BatchY = self.sampleRandom(T_DataY, batch_size = self.batch_size)
                     S_BatchX = self.sampleRandom(S_DataX, batch_size = self.batch_size)                
@@ -112,29 +141,31 @@ class GradientMatching():
                     D.backward()
                     #print('distance ', D)
                     self.optimizerS.step()
-                tempLossLst = []
                 Whole_S = torch.utils.data.TensorDataset(self.S_x, self.S_y)
                 S_loader = torch.utils.data.DataLoader(Whole_S
                                                         , batch_size = batch_size
                                                         , shuffle = True
                                                         , num_workers = 0)
-                #below makes the changes the output shape of fc.
+                tempLossLst = []
                 print('Training on whole S...')
                 for batch, (data, target) in enumerate(S_loader, 1):
                     self.optimizerT.zero_grad() # a clean up step for PyTorch
                     out = model(data.type(torch.float32).to(self.device))
                     out = out.flatten()
                     loss = self.loss_Fun(out, (target).type(torch.float32).to(self.device))
-                    tempLossLst.append(loss.item())
                     loss.backward()
                     self.optimizerT.step()
+                    tempLossLst.append(loss.item())
                     if batch % 2 == 0: #For printing
                         print(4*' ', '===> Training (t): [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
                                 batch * len(data), len(S_loader.dataset),
-                                100. * batch / len(T_x),
+                                (100. * batch) / len(S_loader),
                                 np.mean(tempLossLst)))
+                self.carbonTracker.epoch_end()
                 #temp = torch.sum(torch.eq(old, self.S_x))
                 #print(f'any change? (False = Yes!  True = No!):', temp == 9830400, f'is {temp}')
+        self.carbonTracker.stop()
+                
         return self.S_x, self.S_y
 
 
@@ -225,15 +256,16 @@ class DistributionMatching():
 dataset = 'Alzheimer_MRI'
 datatype     = ''
 costumLabel  = '64x64Full'
-os.chdir('/Users/andreamoody/Documents/GitHub/DC-MasterThesis-2023')
+andrea = False
+if andrea:
+    os.chdir('/Users/andreamoody/Documents/GitHub/DC-MasterThesis-2023')
+else:
+    os.chdir('/home/thire399/Documents/School/DC-MasterThesis-2023')
+    
 batch_size   = 64
 ####### PARAMETERS #######
 print('preparing training data...')
 #Train data
-
-#Concat the two types
-#xTrain, yTrain = DP.DataPrep('Data/Proccesed/chest_xray/train/trainnormal.pt', 'Data/Proccesed/chest_xray/train/trainpneumonia.pt')
-
 xTrain = torch.load(f'Data/Proccesed/{dataset}/trainX.pt')
 yTrain = torch.load(f'Data/Proccesed/{dataset}/trainY.pt')
 xTrain = xTrain.repeat(1, 3, 1, 1)
@@ -245,18 +277,22 @@ train_Loader = torch.utils.data.DataLoader(train_Set,
                                         num_workers = 0)
 
 
-model = M.CD_temp()
+model = M.ConvNet()#M.CD_temp()
 print('\nStaring Condensation...\n')
 GM = GradientMatching(model
                         , batchSize = 64
                         , syntheticSampleSize = 400
-                        , k = 4
-                        , t = 30
+                        , k = 10
+                        , t = 60
                         , c = 2
-                        , lr_Theta = 1e-3
-                        , lr_S = 1e-3
-                        , loss_Fun = nn.BCEWithLogitsLoss())
+                        , lr_Theta = 1e-4
+                        , lr_S = 1.0
+                        , loss_Fun = nn.BCEWithLogitsLoss()
+                        , DataSet = dataset
+                        , customLabel = costumLabel)
+
 x, y = GM.Generate(xTrain, yTrain)
+GM.save_output()
 x = x.cpu().detach().numpy()
 print(y[0])
 plt.imshow(x[0][0], cmap = 'gray')

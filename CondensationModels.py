cf6d66c493929a7b45c8d8565372440128ed3764
AndreaMoody <andrea@moody-vinther.dk>
2023-04-30 21:52:15 +0200

    DM model

--- CondensationModels.py
+++ CondensationModels.py
@@ -176,6 +176,7 @@ class DistributionMatching():
 
     def __init__(self, model, k:int, c:int,  batchSize:int, syntheticSampleSize :int
                  ,loss_Fun, lr_S:float, DataSet:str, customLabel:str) -> None:
+        self.model = model
         self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
         self.model = model.to(self.device)
         self.batch_size = batchSize
@@ -208,7 +209,7 @@ class DistributionMatching():
             rotated_images.append(rotated_image)
         return rotated_images
 
-    def flip_images(images):
+    def flip_images(self, images ):
         flipped_images = []
         for image in images:
             # Convert tensor to PIL Image
@@ -243,48 +244,67 @@ class DistributionMatching():
     def sampleRandom(self, data, batch_size):
         index = np.random.randint(data.shape[0], size = batch_size)
         return torch.stack([data[i] for i in index])
-    def ComputeGrad(self, x):
-        self.model.train()
-        out = self.model(x.to(self.device))
-        out = out.flatten()
-        return out 
+    
+#    def Compute(self, x):
+#        self.model.train()
+#        #print(x.shape)
+#        out = self.model(x.to(self.device))
+#        out = out.flatten()
+#        return out 
     
     def Generate(self, T_x, T_y,):
         Schange_class_index = torch.argmax(self.S_y).item()
         Tchange_class_index = torch.argmax(T_y).item()
+        #net = self.model(output_layer='avgpool') # modify the model to have a global average pooling layer
+        #net(device=self.device) # move the model to the same device as your data
+        embed = self.model.module.avgpool if torch.cuda.device_count() > 1 else self.model.avgpool # for GPU parallel
+        self.model.to(self.device)
         for k in range(self.k):
+            #Sample paratameters for network. 
             self.model._init_weights()
             Loss_Sum = 0
             for c in range(self.c):
                 print('Create Mini Batches')
                 if c == 0:
-                    T_DataX = torch.tensor(T_x[:Tchange_class_index])
-                    T_DataY = torch.tensor(T_y[:Tchange_class_index])
-                    S_DataX = torch.tensor(self.S_x[:Schange_class_index])
-                    S_DataY = torch.tensor(self.S_y[:Schange_class_index])
-                    
+                    T_DataX = (T_x[:Tchange_class_index])
+                    T_DataY = (T_y[:Tchange_class_index])
+                    S_DataX = (self.S_x[:Schange_class_index])
+                    S_DataY = (self.S_y[:Schange_class_index])
+                    #sample w_c - omega for every class
                 else:
-                    T_DataX = torch.tensor(T_x[Tchange_class_index:])
-                    T_DataY = torch.tensor(T_y[Tchange_class_index:])
-                    S_DataX = torch.tensor(self.S_x[Schange_class_index:])
-                    S_DataY = torch.tensor(self.S_y[Schange_class_index:])
+                    T_DataX = (T_x[Tchange_class_index:])
+                    T_DataY = (T_y[Tchange_class_index:])
+                    S_DataX = (self.S_x[Schange_class_index:])
+                    S_DataY = (self.S_y[Schange_class_index:])
+                    #sample w_c - omega for every class
                 print('Sampling...')
-                T_BatchX = self.sampleRandom(T_DataX, batch_size = batch_size)
-                T_BatchY = self.sampleRandom(T_DataY, batch_size = batch_size)
-                S_BatchX = self.sampleRandom(S_DataX, batch_size = batch_size)                
-                S_BatchY = self.sampleRandom(S_DataY, batch_size = batch_size)
+                T_BatchX = self.sampleRandom(T_DataX, batch_size = self.batch_size)
+                T_BatchY = self.sampleRandom(T_DataY, batch_size = self.batch_size)
+                S_BatchX = self.sampleRandom(S_DataX, batch_size = self.batch_size)                
+                S_BatchY = self.sampleRandom(S_DataY, batch_size = self.batch_size)
                 
-                #add augmentation
-                T_out = self.ComputeGrad(T_BatchX)
-                S_out = self.ComputeGrad(S_BatchX)
-                temp_sum_T = torch.mean(T_out)
-                temp_sum_S = torch.mean(S_out) 
-                loss = torch.square(torch.abs(temp_sum_T - temp_sum_S))
+                #compute MDD and add augmentation
+                #T_out = self.Compute(T_BatchX)
+                #S_out = self.Compute(S_BatchX)
+                #print(T_BatchX[1][1])
+                #print(T_BatchX)
+
+                T_aug = TF.rotate(T_BatchX, 0.15)
+                S_aug = TF.rotate(S_BatchX, 0.15)
+                # get embeddings
+                T_embed = embed(T_aug)
+                S_embed = embed(S_aug)
+
+
+                # compute the loss
+                loss = torch.sum((torch.mean(T_embed, dim=0) - torch.mean(S_embed, dim=0))**2)
                 Loss_Sum += loss
+
+            # backpropagation and weight update
+            self.optimizer.zero_grad()
             Loss_Sum.backward()
             self.optimizer.step()
-                #old = S_BatchX
-        return self.S_x
+        return self.S_x, self.S_y
 
 
 ####### PARAMETERS #######
@@ -313,7 +333,7 @@ train_Loader = torch.utils.data.DataLoader(train_Set,
                                         num_workers = 0)
 
 
-model = M.ConvNet()#M.CD_temp()
+#model = M.ConvNet()#M.CD_temp()
 print('\nStaring Condensation...\n')
 #GM = GradientMatching(model
 #                        , batchSize = 64
@@ -326,7 +346,7 @@ print('\nStaring Condensation...\n')
 #                        , loss_Fun = nn.BCEWithLogitsLoss()
 #                        , DataSet = dataset
 #                        , customLabel = costumLabel)
-
+model = M.ConvNet2(output_layer='avgpool')#M.CD_temp()
 DM = DistributionMatching(model
                         , batchSize = 32
                         , syntheticSampleSize = 200

5f26373b4d69f8910b8c623322b063efe4eb2797
AndreaMoody <andrea@moody-vinther.dk>
2023-04-30 22:01:48 +0200

    Squashed commit of the following:
    commit cf6d66c493929a7b45c8d8565372440128ed3764
    Author: AndreaMoody <andrea@moody-vinther.dk>
    Date:   Sun Apr 30 21:52:15 2023 +0200
    
        DM model
    
    commit e7881fe550d50b691800346ace6fd521f320bf63
    Author: AndreaMoody <andrea@moody-vinther.dk>
    Date:   Thu Apr 27 22:02:50 2023 +0200
    
        Update CondensationModels.py
    
    commit a564957bb9c72fd608d0b3bc6fa708371aa2be0a
    Author: AndreaMoody <andrea@moody-vinther.dk>
    Date:   Thu Apr 27 21:47:10 2023 +0200
    
        Update CondensationModels.py
    
    commit fc8f2296a477bfe692b6166c3f3837d5b7d62772
    Author: AndreaMoody <andrea@moody-vinther.dk>
    Date:   Thu Apr 27 21:40:32 2023 +0200
    
        Update CondensationModels.py

--- CondensationModels.py
+++ CondensationModels.py
@@ -177,23 +177,29 @@ class GradientMatching():
 
 class DistributionMatching():
 
-    def __init__(self, model, k = 200, c = 2,  batchSize = 64, syntheticSampleSize = 200
-                 , loss_Fun = nn.BCEWithLogitsLoss(), lr_S = 1e-3):
+    def __init__(self, model, k:int, c:int,  batchSize:int, syntheticSampleSize :int
+                 ,loss_Fun, lr_S:float, DataSet:str, customLabel:str) -> None:
         self.model = model
+        self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+        self.model = model.to(self.device)
+        self.batch_size = batchSize
         self.k = k
         self.c = c
-        self.batch_size = batchSize
+        self.savePath = f'Data/Synthetic_{DataSet}'
+        os.makedirs(f'{self.savePath}/CarbonLogs', exist_ok = True)
+        self.customLabel = customLabel
+        self.lr_S = lr_S
         self.S_x = nn.Parameter(torch.rand((syntheticSampleSize, 3, 128, 128)), requires_grad = True) #Totally random data
         self.S_y = Gen_Y(self.S_x.shape[0])
         self.loss_Fun = loss_Fun
         self.optimizer = optim.SGD([self.S_x], lr = lr_S, momentum = 0.5)
-        self.carbonTracker = CarbonTracker(epochs = self.k*self.t, 
+        self.carbonTracker = CarbonTracker(epochs = self.k, 
                             log_dir = self.savePath + '/CarbonLogs',
                             log_file_prefix = costumLabel + model._get_name(),
                             monitor_epochs = -1,
                             update_interval = 0.01
                             )
-        print(f'Setup:\n\tUsing Compute: {self.device}\n\tk = {k}\n\tt = {t}\n\tc = {c}\n\tLearning Rate S: = {lr_S}')
+        print(f'Setup:\n\tUsing Compute: {self.device}\n\tk = {k}\n \tc = {c}\n\tLearning Rate S: = {lr_S}')
     def rotate_images(images):
         rotated_images = []
         for image in images:
@@ -206,7 +212,7 @@ class DistributionMatching():
             rotated_images.append(rotated_image)
         return rotated_images
 
-    def flip_images(images):
+    def flip_images(self, images ):
         flipped_images = []
         for image in images:
             # Convert tensor to PIL Image
@@ -241,48 +247,67 @@ class DistributionMatching():
     def sampleRandom(self, data, batch_size):
         index = np.random.randint(data.shape[0], size = batch_size)
         return torch.stack([data[i] for i in index])
-    def ComputeGrad(self, x):
-        self.model.train()
-        out = self.model(x.to(self.device))
-        out = out.flatten()
-        return out 
     
-    def DM(self, T_x, T_y, S_x, S_y,):
-        Schange_class_index = torch.argmax(S_y).item()
+#    def Compute(self, x):
+#        self.model.train()
+#        #print(x.shape)
+#        out = self.model(x.to(self.device))
+#        out = out.flatten()
+#        return out 
+    
+    def Generate(self, T_x, T_y,):
+        Schange_class_index = torch.argmax(self.S_y).item()
         Tchange_class_index = torch.argmax(T_y).item()
+        #net = self.model(output_layer='avgpool') # modify the model to have a global average pooling layer
+        #net(device=self.device) # move the model to the same device as your data
+        embed = self.model.module.avgpool if torch.cuda.device_count() > 1 else self.model.avgpool # for GPU parallel
+        self.model.to(self.device)
         for k in range(self.k):
+            #Sample paratameters for network. 
             self.model._init_weights()
             Loss_Sum = 0
             for c in range(self.c):
                 print('Create Mini Batches')
                 if c == 0:
-                    T_DataX = torch.tensor(T_x[:Tchange_class_index])
-                    T_DataY = torch.tensor(T_y[:Tchange_class_index])
-                    S_DataX = torch.tensor(S_x[:Schange_class_index])
-                    S_DataY = torch.tensor(S_y[:Schange_class_index])
-                    
+                    T_DataX = (T_x[:Tchange_class_index])
+                    T_DataY = (T_y[:Tchange_class_index])
+                    S_DataX = (self.S_x[:Schange_class_index])
+                    S_DataY = (self.S_y[:Schange_class_index])
+                    #sample w_c - omega for every class
                 else:
-                    T_DataX = torch.tensor(T_x[Tchange_class_index:])
-                    T_DataY = torch.tensor(T_y[Tchange_class_index:])
-                    S_DataX = torch.tensor(S_x[Schange_class_index:])
-                    S_DataY = torch.tensor(S_y[Schange_class_index:])
+                    T_DataX = (T_x[Tchange_class_index:])
+                    T_DataY = (T_y[Tchange_class_index:])
+                    S_DataX = (self.S_x[Schange_class_index:])
+                    S_DataY = (self.S_y[Schange_class_index:])
+                    #sample w_c - omega for every class
                 print('Sampling...')
-                T_BatchX = self.sampleRandom(T_DataX, batch_size = batch_size)
-                T_BatchY = self.sampleRandom(T_DataY, batch_size = batch_size)
-                S_BatchX = self.sampleRandom(S_DataX, batch_size = batch_size)                
-                S_BatchY = self.sampleRandom(S_DataY, batch_size = batch_size)
+                T_BatchX = self.sampleRandom(T_DataX, batch_size = self.batch_size)
+                T_BatchY = self.sampleRandom(T_DataY, batch_size = self.batch_size)
+                S_BatchX = self.sampleRandom(S_DataX, batch_size = self.batch_size)                
+                S_BatchY = self.sampleRandom(S_DataY, batch_size = self.batch_size)
                 
-                #add augmentation
-                T_out = self.ComputeGrad(T_BatchX)
-                S_out = self.ComputeGrad(S_BatchX)
-                temp_sum_T = torch.mean(T_out)
-                temp_sum_S = torch.mean(S_out) 
-                loss = torch.square(torch.abs(temp_sum_T - temp_sum_S))
+                #compute MDD and add augmentation
+                #T_out = self.Compute(T_BatchX)
+                #S_out = self.Compute(S_BatchX)
+                #print(T_BatchX[1][1])
+                #print(T_BatchX)
+
+                T_aug = TF.rotate(T_BatchX, 0.15)
+                S_aug = TF.rotate(S_BatchX, 0.15)
+                # get embeddings
+                T_embed = embed(T_aug)
+                S_embed = embed(S_aug)
+
+
+                # compute the loss
+                loss = torch.sum((torch.mean(T_embed, dim=0) - torch.mean(S_embed, dim=0))**2)
                 Loss_Sum += loss
+
+            # backpropagation and weight update
+            self.optimizer.zero_grad()
             Loss_Sum.backward()
             self.optimizer.step()
-                #old = S_BatchX
-        return self.S_x
+        return self.S_x, self.S_y
 
 
 ####### PARAMETERS #######
@@ -293,10 +318,10 @@ datatype     = ''
 costumLabel  = 'After'
 andrea = False
 if andrea:
-    os.chdir('/Users/andreamoody/Documents/GitHub/DC-MasterThesis-2023')
+    os.chdir(r"C:\Users\andre\Documents\GitHub\DC-MasterThesis-2023")
 else:
     os.chdir('/home/thire399/Documents/School/DC-MasterThesis-2023')
-batch_size   = 64
+batch_size   = 32
 ####### PARAMETERS #######
 print('preparing training data...')
 #Train data
@@ -311,25 +336,41 @@ train_Loader = torch.utils.data.DataLoader(train_Set,
                                         num_workers = 0)
 
 
-model = M.ConvNet()#M.CD_temp()
+#model = M.ConvNet()#M.CD_temp()
 print('\nStaring Condensation...\n')
-GM = GradientMatching(model
-                        , batchSize = 64
-                        , syntheticSampleSize = 50
-                        , k = 10
-                        , t = 50
+#GM = GradientMatching(model
+#                        , batchSize = 64
+#                        , syntheticSampleSize = 50
+#                        , k = 10
+#                        , t = 50
+#                        , c = 2
+#                        , lr_Theta = 0.01
+#                        , lr_S = 0.1
+#                        , loss_Fun = nn.BCEWithLogitsLoss()
+#                        , DataSet = dataset
+#                        , customLabel = costumLabel)
+model = M.ConvNet2(output_layer='avgpool')#M.CD_temp()
+DM = DistributionMatching(model
+                        , batchSize = 32
+                        , syntheticSampleSize = 200
+                        , k = 200
                         , c = 2
-                        , lr_Theta = 0.01
-                        , lr_S = 0.1
+                        #, lr_Theta = 0.01
+                        , lr_S = 1e-3
                         , loss_Fun = nn.BCEWithLogitsLoss()
                         , DataSet = dataset
                         , customLabel = costumLabel)
 
-x, y, d = GM.Generate(xTrain, yTrain)
-GM.save_output()
+#x, y, d = GM.Generate(xTrain, yTrain)
+#GM.save_output()
+
+x = DM.Generate(xTrain, yTrain)
+#DM.save_output()
+
 x = x.cpu().detach().numpy()
-plt.plot(range(len(d)), d)
-plt.show()
+#plt.plot(range(len(d)), d)
+#plt.show()
+
 
 #print(y[0])
 #plt.imshow(x[0][0], cmap = 'gray')

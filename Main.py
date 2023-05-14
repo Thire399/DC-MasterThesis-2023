70bd720ffa076854a128debb8abbf8c35ee4fe5e
AndreaMoody <andrea@moody-vinther.dk>
2023-05-14 16:19:28 +0200

    Update Main.py

--- Main.py
+++ Main.py
@@ -14,8 +14,8 @@ os.chdir(homeDir)
 
 ####### PARAMETERS #######
 
-model = models.densenet169(pretrained = False)
-model.classifier = nn.Linear(in_features=1664, out_features = 1, bias=True)
+#model = models.densenet169(pretrained = False)
+#model.classifier = nn.Linear(in_features=1664, out_features = 1, bias=True)
 
 #model = models.alexnet(pretrained = False)
 #model.classifier[6] = nn.Linear(in_features=4096, out_features = 1, bias=True)
@@ -23,9 +23,12 @@ model.classifier = nn.Linear(in_features=1664, out_features = 1, bias=True)
 #model.classifier[1] = nn.Linear(in_features=1280, out_features = 1, bias=True)
 #model = models.inception_v3(pretrained = False)
 #model.fc = nn.Linear(in_features=2048, out_features = 1, bias=True)
-
-#model = models.resnet50(pretrained = False)
-#model.fc = nn.Linear(in_features = 2048, out_features = 1, bias = True)
+#model = M.ConvNet()
+#model = M.VGG()
+#model = M.ResNet2()
+model = M.DenseNet2()
+#model = models.resnet18(pretrained = False)
+#model.fc = nn.Linear(in_features = 512, out_features = 1, bias = True)
 
 #model.fc.add_module('Sigmoid', nn.Sigmoid())
 #model = M.UNet(enc_chs = (3, 64, 128, 256, 512, 1024)
@@ -36,11 +39,12 @@ model.classifier = nn.Linear(in_features=1664, out_features = 1, bias=True)
 #Data parameters
 synthetic = True
 dataSet      = 'Alzheimer_MRI'
+#dataSet = 'MNIST'
 #dataSet      = 'chest_xray'
-datatype     = 'DMAfter_LR01_k20_nor_2016_bs10'
+datatype     = 'NEW_DMAfterAL_LR1_k20_100_even_402_2' #    DMAfter_LR1_k20_402_even  DMAfter_LR01_k20_nor_2016_bs10
 #'10PercentDistribution'
-costumLabel  = 'SyntheticMRI128x128_bs10_adam_lr5'#DMAfter_LR01_k20
-#costumLabel = '64x6410PercentDistribution'
+costumLabel  = 'NEW_DMAfterAL_LR1_k20_100_even_402_2_dens'#DMAfter_LR01_k20   DMAfterMNIST_LR1_k20_100_evenBeforeX
+#costumLabel = '64x6410PercentDistributionNew'  # use this 
 
 dev = True
 #model parameters
@@ -48,7 +52,7 @@ patience     = 10 #
 delta        = 1e-4
 epochs       = 400
 
-learningRate = 1e-5
+learningRate = 1e-3
 optimizer    = optim.SGD(model.parameters(), lr = learningRate, momentum = 0.9)
 #optimizer    =  optim.Adam(model.parameters(), lr = learningRate)
 loss_Fun     = nn.BCEWithLogitsLoss()
@@ -70,6 +74,20 @@ def min_max_normalization(images_tensor):
     # Normalize the tensor using min-max normalization 
     normalized_tensor = (images_tensor - min_value) / (max_value - min_value) 
     return normalized_tensor
+
+
+def min_max_normalization2(images_tensor):
+    # Iterate over each tensor in the input tensor
+    for i in range(images_tensor.shape[0]):
+        tensor = images_tensor[i]
+        # Calculate the minimum and maximum values for the current tensor
+        min_value = torch.min(tensor)
+        max_value = torch.max(tensor)
+        # Normalize the tensor using min-max normalization
+        normalized_tensor = (tensor - min_value) / (max_value - min_value)
+        # Assign the normalized tensor back to the input tensor
+        images_tensor[i] = normalized_tensor
+    return images_tensor
     
 def __main__():
         print('Starting...')
@@ -77,7 +95,7 @@ def __main__():
             xTrain = torch.load(f'Data/Synthetic_{dataSet}/' + datatype + 'X.pt')
             yTrain = torch.load(f'Data/Synthetic_{dataSet}/' + datatype + 'Y.pt')
             with torch.no_grad():
-                xTrain = min_max_normalization(xTrain.repeat(1,3,1,1))
+                xTrain = min_max_normalization2(xTrain)#.repeat(1,3,1,1))
         elif highRes:
             import Data_processing as DP
             print('High resulotion...\nGetting file names...')
@@ -91,12 +109,12 @@ def __main__():
         else:
             xTrain = torch.load('Data/Proccesed/'+ dataSet +'/' + datatype + 'trainX.pt')
             yTrain = torch.load('Data/Proccesed/'+ dataSet +'/' + datatype + 'trainY.pt')
-            xTrain = xTrain.repeat(1, 3, 1, 1) # only for pretrained model
+            xTrain = xTrain#.repeat(1, 3, 1, 1) # only for pretrained model
         if dataSet == 'chest_xray':
             if not highRes:
                 xVal = torch.load('Data/Proccesed/'+ dataSet + '/tempValX.pt')
                 yVal = torch.load('Data/Proccesed/'+ dataSet + '/tempValY.pt')
-                xVal = xVal.repeat(1, 3, 1, 1)     # only for pretrained model
+                xVal = xVal#.repeat(1, 3, 1, 1)     # only for pretrained model
             else:
                 files = DP.GetFileNames('Data/Proccesed/chest_xray/temporaryVal/')
                 val_Set = DP.ChestXrayDataset(files = files)
@@ -108,7 +126,7 @@ def __main__():
         else:
             xVal = torch.load('Data/Proccesed/'+ dataSet + '/ValX.pt')
             yVal = torch.load('Data/Proccesed/'+ dataSet + '/ValY.pt')
-            xVal = xVal.repeat(1, 3, 1, 1)     # only for pretrained model
+            xVal = xVal#.repeat(1, 3, 1, 1)     # only for pretrained model
         
         if not highRes:
             train_Set = torch.utils.data.TensorDataset(xTrain, yTrain)
